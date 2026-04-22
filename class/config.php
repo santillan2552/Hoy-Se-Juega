ab36999d42f6db59827e75977b2db16bfa72249a
santillan2552 <santillan2552@gmail.com>
2026-04-22 13:15:43 -0300

    Update config.php

--- class/config.php
+++ class/config.php
@@ -1,16 +1,16 @@
 <?php
 define('SKIN_LANGUAGE', 'skins_en');
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', '45.235.98.222');
 define('DB_PORT', '3306');
-define('DB_NAME', '');
-define('DB_USER', '');
-define('DB_PASS', '');
+define('DB_NAME', 's1244_css');
+define('DB_USER', 'u1244_6TZ9T6P2MQ');
+define('DB_PASS', 'LT9@1SrBedjZnqI9!JsfBZTv');
 
 define('WEB_STYLE_DARK', true);
 
-define('STEAM_API_KEY', '');
-define('STEAM_DOMAIN_NAME', '');
-define('STEAM_LOGOUT_PAGE', '');
-define('STEAM_LOGIN_PAGE', '');
+define('STEAM_API_KEY', '9DC0D1204D5B5601BB89A064EE6588DF');
+define('STEAM_DOMAIN_NAME', 'hoy-se-juega-production.up.railway.app');
+define('STEAM_LOGOUT_PAGE', 'httpps://hoy-se-juega-production.up.railway.app');
+define('STEAM_LOGIN_PAGE', 'httpps://hoy-se-juega-production.up.railway.app');
 
